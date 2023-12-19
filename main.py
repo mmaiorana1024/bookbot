e2c4e0da7606b3819888dae9be1898f199c5d71c
mmaiorana1024 <mmaiorana1024@gmail.com>
2023-12-19 13:14:33 -0500

    added report part of project

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n\n")
     letter_count = get_count_letters(text)
-    print(f"{letter_count}")
+    get_list_of_char(letter_count)
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -25,6 +27,18 @@ def get_count_letters(text):
             else:
                 letter_count[letter.lower()] = 1
     return letter_count
-                 
+
+def get_list_of_char(text):
+    list_of_letters = []
+    
+    for letter in text:
+        if letter.isalpha():
+          list_of_letters.append(letter)
+    
+    list_of_letters.sort()
+    
+    for letter in range(0, len(list_of_letters)):
+        print(f"The '{list_of_letters[letter]}' character was found {text[list_of_letters[letter]]} times")
+    
 
 main()
